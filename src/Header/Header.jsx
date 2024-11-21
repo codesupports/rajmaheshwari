519fa6c46962f4f22800bc7352925be816aea9d2
codesupports <raj.mah55@gmail.com>
2024-11-21 22:30:54 +0530

    forth commit

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import './Header.scss'
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 const Header = () => {
+
+    const [toggle, setToggle] = useState(false);
+    const handleCheckboxChange = () => {
+        setToggle(!toggle)
+    }
+
     return (
-        <header className="header">
-            <ul >
+        <header className="header ">
+            <ul className='d-desktop'>
                 <li className='desktop-nav'>
                     <Link to="/">
                         <div className=''>
@@ -29,6 +36,34 @@ const Header = () => {
                     </Link>
                 </li>
             </ul>
+            <nav className={`hidden d-mobile mobile-nav ${toggle == true ? "showSideNav" : "hideSideNav"}`}>
+                <section>
+                    <div className={`inputToggle`}>
+                        <input type="checkbox" onChange={handleCheckboxChange} />
+                        <span className='white-line' />
+                        <span className='white-line' />
+                        <span className='white-line' />
+                    </div>
+                    <ul>
+                        <li onClick={handleCheckboxChange} >
+                            <Link to="/">
+                                <div><i className='fa fa-home' /><span>Home</span></div>
+                            </Link>
+                        </li>
+                        <li onClick={handleCheckboxChange}>
+                            <Link to="/About">
+                                <div><i className='fa fa-user' /><span>About</span></div>
+                            </Link>
+                        </li>
+                        <li onClick={handleCheckboxChange}>
+                            <Link to="/Contact">
+                                <div><i className='fa fa-envelope-open' /><span>Contact</span></div>
+                            </Link>
+                        </li>
+                    </ul>
+                </section>
+
+            </nav>
         </header>
     )
 }
