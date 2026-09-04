86c4d6bf73caa9b79d3124ec9ef3ea4e42ebcb6c
Raj Kumar Maheshwari <85394585+codesupports@users.noreply.github.com>
2026-09-04 22:13:26 +0530

    Update Home.jsx
    Home changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -16,17 +16,27 @@ const Home = () => {
                             <li>I'm</li>
                             <li>React JS</li>
                             <li>JavaScript</li>
-                            <li>HTML5</li>
-                            <li>CSS</li>
+                            <li>Next JS</li>
+                            <li>TypeScript</li>
+                            <li>Redux Toolkit</li>
+                            <li>Rest API</li>
+                            <li>HTML/CSS/Tailwind</li>
                             <li>RWD</li>
-                            <li>SCSS</li>
-                            <li>Node</li>
+                            <li>Github Copilot</li>
                         </ul>
                     </div>
                     <div className="pannel__right">
                         <h1>Raj Maheshwari</h1>
                         <h2 className='designation'><span className='spin'></span> <span> Developer</span></h2>
-                        <p>I am an experienced Frontend React.js Developer with over 8 years of expertise in creating responsive and high-performance web applications. My proficiency in <span className='highlight'>HTML, CSS, JavaScript and React.js,</span> combined with deep knowledge of state management tools like Redux, Redux-Toolkit and hooks, allows me to build scalable, maintainable, and optimized user interfaces. I am passionate about leveraging the latest frontend technologies to deliver exceptional user experiences.</p>
+                        <p>
+                            Senior Frontend Engineer with 13 years of total experience including 6 years of Web Graphic Design and <span className='highlight'>7 years of relevant experience in React Frontend Development</span>. Experienced in designing and building
+                            scalable, high-performance web applications using <span className='highlight'>React.js, Next.js, TypeScript, JavaScript (ES6+),
+                                Redux Toolkit, and modern frontend technologies </span>. Expertise in frontend architecture, reusable
+                            component libraries, responsive web applications, REST API integration, performance optimization,
+                            state management, and WCAG-compliant accessibility, with a strong focus on delivering exceptional
+                            user experiences.
+                        </p>
+                        {/* <p>I am an experienced Frontend React.js Developer with over 8 years of expertise in creating responsive and high-performance web applications. My proficiency in <span className='highlight'>HTML, CSS, JavaScript and React.js,</span> combined with deep knowledge of state management tools like Redux, Redux-Toolkit and hooks, allows me to build scalable, maintainable, and optimized user interfaces. I am passionate about leveraging the latest frontend technologies to deliver exceptional user experiences.</p> */}
                         <Link to="/About" className='more'>MORE ABOUT ME</Link>
                         <div className="social-icon">
                             <span>
